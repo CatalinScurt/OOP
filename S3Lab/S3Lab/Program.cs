c4d3e810b4a3ade23c4346515d4897dc849dbd98
Catalin Scurt <cata_scurt@yahoo.com>
2021-03-18 00:05:13 +0200

    add Laborator 3

---
+++ S3Lab/S3Lab/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace S3Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti n:");
+            int n = int.Parse(Console.ReadLine());
+            string[] v = new string[n];
+            Star.View(n, v);
+            GC.Collect();
+        }
+        class Star
+        {
+            Star star = new Star();
+          public static string[] Stea(int n, string[] v)
+            {
+                for (int i = 0; i <= n; i++)
+                    for (int j = i; j < n; j++)
+                        v[j] += "*";
+                return v;
+            }
+            public static void View(int n, string[] v)
+            {
+                foreach (string item in Stea(n, v))
+                    Console.WriteLine(item);
+            }
+            ~Star()
+            {
+                Console.WriteLine("Class Star was destroyed"); 
+            }
+        }
+
+    }
+}
